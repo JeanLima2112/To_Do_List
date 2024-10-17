b6e29ce8a05f91979dc54e28f3cc7c5094bd5ea9
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-10-16 22:23:04 -0300

    fix bug

--- To_do_list_Mobile/src/app/register/register.page.ts
+++ To_do_list_Mobile/src/app/register/register.page.ts
@@ -10,58 +10,52 @@ import { UserService } from './register.service';
   styleUrls: ['register.page.scss'],
 })
 export class RegisterPage {
-  name: string = '';  
+  name: string = '';
   username: string = '';
   password: string = '';
   isLoading: boolean = false;
 
   constructor(
-    private router: Router, 
-    private userService: UserService, 
-    private authService: AuthService, 
+    private router: Router,
+    private userService: UserService,
+    private authService: AuthService,
     private toastController: ToastController
   ) {}
 
   async onRegister() {
     this.isLoading = true;
 
-    this.userService.createUser(this.name, this.username, this.password).subscribe(
-      async response => {
-        console.log('Usuário criado com sucesso:', response);
+    this.userService
+      .createUser(this.name, this.username, this.password)
+      .subscribe(
+        async (response) => {
+          console.log('Usuário criado com sucesso:', response);
 
-        
-        this.authService.login(this.username, this.password).subscribe(
-          async loginResponse => {
-            console.log('Login bem-sucedido!', loginResponse);
-            await this.showToast('Usuário criado e logado com sucesso!');
-            this.router.navigate(['/home']); 
-          },
-          async loginError => {
-            console.error('Erro no login', loginError);
-            await this.showToast('Usuário criado, mas erro ao logar.'); 
-          }
-        );
-      },
-      async error => {
-        console.error('Erro ao criar usuário:', error);
-        await this.showToast('Erro ao criar usuário: ' + (error.error.message || 'Tente novamente.'));
-      },
-      () => {
-        this.isLoading = false;
-      }
-    );
+          this.authService.loginAndStore(this.username, this.password);
+          this.router.navigate(['/home']);
+        },
+        async (error) => {
+          console.error('Erro ao criar usuário:', error);
+          await this.showToast(
+            'Erro ao criar usuário: ' +
+              (error.error.message || 'Tente novamente.')
+          );
+        },
+        () => {
+          this.isLoading = false;
+        }
+      );
   }
 
   goToLogin() {
     this.router.navigate(['/login']);
   }
 
-
   async showToast(message: string) {
     const toast = await this.toastController.create({
       message: message,
       duration: 3000,
-      position: 'bottom'
+      position: 'bottom',
     });
     toast.present();
   }
